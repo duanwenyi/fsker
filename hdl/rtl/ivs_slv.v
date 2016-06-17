5685186e4b634a22a7a9bbc42408bee898cb7be0
Apachee <duanwenyi880@163.com>
2016-06-17 10:43:15 -0400

    simple Makefile

--- hdl/rtl/ivs_slv.v
+++ hdl/rtl/ivs_slv.v
@@ -9,128 +9,128 @@ module IVS_SLV(/*AUTOARG*/
    hprot, hready_in
    );
 
-   input                hclk;
-   input 				hrst_n;
-   
-   // AHB BUS
-   input 				hsel;      
-   input [1:0] 			htrans;      
-   input 				hwrite;      
-   input [31:0] 		haddr;       
-   input [31:0] 		hwdata;      
-   input [2:0] 			hsize;     
-   input [2:0] 			hburst;    
-   input [3:0] 			hprot;     
-   input 				hready_in;
-   
-   output 				hready_out;
-   output [1:0] 		hresp; 
-   output reg [31:0] 	hrdata;
+    input                hclk;
+    input                hrst_n;
+    
+    // AHB BUS
+    input                hsel;      
+    input [1:0]          htrans;      
+    input                hwrite;      
+    input [31:0]         haddr;       
+    input [31:0]         hwdata;      
+    input [2:0]          hsize;     
+    input [2:0]          hburst;    
+    input [3:0]          hprot;     
+    input                hready_in;
+    
+    output               hready_out;
+    output [1:0]         hresp; 
+    output reg [31:0]    hrdata;
 
-   output reg [31:0] 	cfg_par0;
-   output reg [31:0] 	cfg_par1;
-   output reg [31:0] 	cfg_par2;
-   output reg [31:0] 	cfg_par3;
-   output reg [31:0] 	cfg_par4;
-   output reg [31:0] 	cfg_par5;
-   output reg [31:0] 	cfg_par6;
-   output reg [31:0] 	cfg_par7;
+    output reg [31:0]    cfg_par0;
+    output reg [31:0]    cfg_par1;
+    output reg [31:0]    cfg_par2;
+    output reg [31:0]    cfg_par3;
+    output reg [31:0]    cfg_par4;
+    output reg [31:0]    cfg_par5;
+    output reg [31:0]    cfg_par6;
+    output reg [31:0]    cfg_par7;
 
-   output reg [31:0] 	glb_ctrl;
-   output reg 			sw_rst;
+    output reg [31:0]    glb_ctrl;
+    output reg           sw_rst;
 
-   reg [9:0] 			addr_ofst;
+    reg [9:0]            addr_ofst;
 
-   reg 					cs_en_ff;
-   reg 					wr_en_ff;
-   reg 					rd_en_ff;
+    reg                  cs_en_ff;
+    reg                  wr_en_ff;
+    reg                  rd_en_ff;
 
-   wire 				cs_en = hsel & (htrans != 2'b0) & hready_in;
-   wire 				wr_en = cs_en &  hwrite;
-   wire 				rd_en = cs_en & ~hwrite;
+    wire                 cs_en = hsel & (htrans != 2'b0) & hready_in;
+    wire                 wr_en = cs_en &  hwrite;
+    wire                 rd_en = cs_en & ~hwrite;
 
-   wire [31:0] 			hrdata_s = ( {32{addr_ofst == `IVS_CTRL}} & glb_ctrl |
-      
-									 {32{addr_ofst == `IVS_PAR0}} & cfg_par0 |
-									 {32{addr_ofst == `IVS_PAR1}} & cfg_par1 |
-									 {32{addr_ofst == `IVS_PAR2}} & cfg_par2 |
-									 {32{addr_ofst == `IVS_PAR3}} & cfg_par3 |
-									 {32{addr_ofst == `IVS_PAR4}} & cfg_par4 |
-									 {32{addr_ofst == `IVS_PAR5}} & cfg_par5 |
-									 {32{addr_ofst == `IVS_PAR6}} & cfg_par6 |
-									 {32{addr_ofst == `IVS_PAR7}} & cfg_par7
-									 );
+    wire [31:0]          hrdata_s = ( {32{addr_ofst == `IVS_CTRL}} & glb_ctrl |
+        
+									  {32{addr_ofst == `IVS_PAR0}} & cfg_par0 |
+									  {32{addr_ofst == `IVS_PAR1}} & cfg_par1 |
+									  {32{addr_ofst == `IVS_PAR2}} & cfg_par2 |
+									  {32{addr_ofst == `IVS_PAR3}} & cfg_par3 |
+									  {32{addr_ofst == `IVS_PAR4}} & cfg_par4 |
+									  {32{addr_ofst == `IVS_PAR5}} & cfg_par5 |
+									  {32{addr_ofst == `IVS_PAR6}} & cfg_par6 |
+									  {32{addr_ofst == `IVS_PAR7}} & cfg_par7
+									  );
 
-   wire 				sw_rst_s = wr_en_ff && (addr_ofst == `IVS_TRIG) && hwdata[0];
+    wire                 sw_rst_s = wr_en_ff && (addr_ofst == `IVS_TRIG) && hwdata[0];
 
-   assign hready_out = ~cs_en_ff;
-   assign hresp      = 2'b0;
+    assign hready_out = ~cs_en_ff;
+    assign hresp      = 2'b0;
 
-   always @(posedge hclk)
-     if(~hrst_n)
-       wr_en_ff  <= 1'b0;
-     else if(wr_en^wr_en_ff)
-       wr_en_ff  <= wr_en;
-   
-   always @(posedge hclk)
-     if(~hrst_n)
-       rd_en_ff  <= 1'b0;
-     else if(rd_en^rd_en_ff)
-       rd_en_ff  <= rd_en;
+    always @(posedge hclk)
+      if(~hrst_n)
+        wr_en_ff  <= 1'b0;
+      else if(wr_en^wr_en_ff)
+        wr_en_ff  <= wr_en;
+    
+    always @(posedge hclk)
+      if(~hrst_n)
+        rd_en_ff  <= 1'b0;
+      else if(rd_en^rd_en_ff)
+        rd_en_ff  <= rd_en;
 
-   always @(posedge hclk)
-     if(~hrst_n)
-       cs_en_ff  <= 1'b0;
-     else if(cs_en^cs_en_ff)
-       cs_en_ff  <= cs_en;
+    always @(posedge hclk)
+      if(~hrst_n)
+        cs_en_ff  <= 1'b0;
+      else if(cs_en^cs_en_ff)
+        cs_en_ff  <= cs_en;
 
-   // haddr[1:0] always be zero , optimized later .
-   always @(posedge hclk)
-     if(~hrst_n)
-       addr_ofst  <= 10'b0;
-     else if(cs_en)
-       addr_ofst  <= haddr[11:0];
+    // haddr[1:0] always be zero , optimized later .
+    always @(posedge hclk)
+      if(~hrst_n)
+        addr_ofst  <= 10'b0;
+      else if(cs_en)
+        addr_ofst  <= haddr[11:0];
 
-   always @(posedge hclk)
-     if(~hrst_n)begin
-	glb_ctrl  <= 32'h0;
-	
-	cfg_par0  <= 32'h0;
-	cfg_par1  <= 32'h0;
-	cfg_par2  <= 32'h0;
-	cfg_par3  <= 32'h0;
-	cfg_par4  <= 32'h0;
-	cfg_par5  <= 32'h0;
-	cfg_par6  <= 32'h0;
-	cfg_par7  <= 32'h0;
-     end
-     else if(wr_en_ff)
-       case(addr_ofst)
-	 10'h000:   glb_ctrl  <= hwdata;
-	 
-	 10'h100:   cfg_par0  <= hwdata;
-	 10'h104:   cfg_par1  <= hwdata;
-	 10'h108:   cfg_par2  <= hwdata;
-	 10'h10c:   cfg_par3  <= hwdata;
-	 10'h110:   cfg_par4  <= hwdata;
-	 10'h114:   cfg_par5  <= hwdata;
-	 10'h118:   cfg_par6  <= hwdata;
-	 10'h11c:   cfg_par7  <= hwdata;
-       endcase // case (addr_ofst)
-   
-   
-   
-   always @(posedge hclk)
-     if(~hrst_n)
-       hrdata  <= 32'b0;
-     else if(rd_en_ff)
-       hrdata  <= hrdata_s;
+    always @(posedge hclk)
+      if(~hrst_n)begin
+	      glb_ctrl  <= 32'h0;
+	      
+	      cfg_par0  <= 32'h0;
+	      cfg_par1  <= 32'h0;
+	      cfg_par2  <= 32'h0;
+	      cfg_par3  <= 32'h0;
+	      cfg_par4  <= 32'h0;
+	      cfg_par5  <= 32'h0;
+	      cfg_par6  <= 32'h0;
+	      cfg_par7  <= 32'h0;
+      end
+      else if(wr_en_ff)
+        case(addr_ofst)
+	        10'h000:   glb_ctrl  <= hwdata;
+	        
+	        10'h100:   cfg_par0  <= hwdata;
+	        10'h104:   cfg_par1  <= hwdata;
+	        10'h108:   cfg_par2  <= hwdata;
+	        10'h10c:   cfg_par3  <= hwdata;
+	        10'h110:   cfg_par4  <= hwdata;
+	        10'h114:   cfg_par5  <= hwdata;
+	        10'h118:   cfg_par6  <= hwdata;
+	        10'h11c:   cfg_par7  <= hwdata;
+        endcase // case (addr_ofst)
+    
+    
+    
+    always @(posedge hclk)
+      if(~hrst_n)
+        hrdata  <= 32'b0;
+      else if(rd_en_ff)
+        hrdata  <= hrdata_s;
 
-   always @(posedge hclk)
-     if(~hrst_n)
-       sw_rst  <= 1'b0;
-     else if(sw_rst_s^sw_rst)
-       sw_rst  <= sw_rst_s;
+    always @(posedge hclk)
+      if(~hrst_n)
+        sw_rst  <= 1'b0;
+      else if(sw_rst_s^sw_rst)
+        sw_rst  <= sw_rst_s;
 
-   
+    
 endmodule // IVS_SLV
--- hdl/tb/test_top.sv
+++ hdl/tb/test_top.sv
@@ -1,194 +1,194 @@
 
 module TH;
 
-   // Clock & reset domain
-   wire 	    aclk;
-   wire 	    hclk;
-   
-   wire 	    arest_n;
-   wire 	    hrest_n;
+    // Clock & reset domain
+    wire 	    aclk;
+    wire 	    hclk;
+    
+    wire 	    arest_n;
+    wire 	    hrest_n;
 
-   // AHB BUS
-   wire [1:0] 	htrans;      
-   wire 	    hwrite;      
-   wire [31:0] 	haddr;       
-   wire [31:0] 	hwdata;      
-   wire [2:0] 	hsize;     
-   wire [2:0] 	hburst;    
-   wire [3:0] 	hprot;     
-   wire 	    hready_out;
-   
-   wire 	    hready_in;
-   wire [1:0] 	hresp; 
-   wire [31:0] 	hrdata;
-   
-   
-   
-   // AXI BUS
-   wire 	    arready;                     
-   wire 	    arvalid;                     
-   wire [5:0] 	arid;        // [3:0]        
-   wire [63:0] 	araddr;                  
-   wire [5:0] 	arlen;       // [5:0]        
-   wire [2:0] 	arsize;      // [2:0]  3'b100
-   wire [1:0] 	arburst;     // [1:0]  2'b01 
-   wire 	    arlock;                      
-   wire [3:0] 	arcache;     // [3:0]        
-   wire [2:0] 	arprot;      // [2:0]        
-   wire [3:0] 	arregion;    // [3:0]        
-   wire [3:0] 	arqos;       // [3:0]        
-   wire [7:0] 	aruser;      // [7:0]        
-   
-   wire 	    rready;                      
-   wire 	    rvalid;                      
-   wire [5:0] 	rid;              // [5:0] 
-   wire [4:0] 	ruser;            // [4:0] 
-   wire [127:0] rdata;            // [31:0]
-   wire 	    rlast;                       
-   wire [1:0] 	rresp;   // [1:0] 
+    // AHB BUS
+    wire [1:0]  htrans;      
+    wire 	    hwrite;      
+    wire [31:0] haddr;       
+    wire [31:0] hwdata;      
+    wire [2:0]  hsize;     
+    wire [2:0]  hburst;    
+    wire [3:0]  hprot;     
+    wire 	    hready_out;
+    
+    wire 	    hready_in;
+    wire [1:0]  hresp; 
+    wire [31:0] hrdata;
+    
+    
+    
+    // AXI BUS
+    wire 	    arready;                     
+    wire 	    arvalid;                     
+    wire [5:0]  arid;        // [3:0]        
+    wire [63:0] araddr;                  
+    wire [5:0]  arlen;       // [5:0]        
+    wire [2:0]  arsize;      // [2:0]  3'b100
+    wire [1:0]  arburst;     // [1:0]  2'b01 
+    wire 	    arlock;                      
+    wire [3:0]  arcache;     // [3:0]        
+    wire [2:0]  arprot;      // [2:0]        
+    wire [3:0]  arregion;    // [3:0]        
+    wire [3:0]  arqos;       // [3:0]        
+    wire [7:0]  aruser;      // [7:0]        
+    
+    wire 	    rready;                      
+    wire 	    rvalid;                      
+    wire [5:0]  rid;              // [5:0] 
+    wire [4:0]  ruser;            // [4:0] 
+    wire [127:0] rdata;            // [31:0]
+    wire         rlast;                       
+    wire [1:0]   rresp;   // [1:0] 
 
 
-   wire 	    awready;                     
-   wire 	    awvalid;                      
-   wire [5:0] 	awid;        // [3:0]         
-   wire [63:0] 	awaddr;                   
-   wire [5:0] 	awlen;       // [5:0]         
-   wire [2:0] 	awsize;      // [2:0]  3'b100 
-   wire [1:0] 	awburst;     // [1:0]  2'b01  
-   wire 	    awlock;                       
-   wire [3:0] 	awcache;     // [3:0]         
-   wire [2:0] 	awprot;      // [2:0]         
-   wire [3:0] 	awregion;    // [3:0]         
-   wire [3:0] 	awqos;       // [3:0]         
-   wire [7:0] 	awuser;      // [7:0]         
-   
-   wire 	    wready;                       
-   wire 	    wvalid;                       
-   wire 	    wlast;                        
-   wire [5:0] 	wid;                // [3:0]  
-   wire [127:0] wdata;            // [31:0]    
-   wire [15:0] 	wstrb ;   // [15:0] 
+    wire         awready;                     
+    wire         awvalid;                      
+    wire [5:0]   awid;        // [3:0]         
+    wire [63:0]  awaddr;                   
+    wire [5:0]   awlen;       // [5:0]         
+    wire [2:0]   awsize;      // [2:0]  3'b100 
+    wire [1:0]   awburst;     // [1:0]  2'b01  
+    wire         awlock;                       
+    wire [3:0]   awcache;     // [3:0]         
+    wire [2:0]   awprot;      // [2:0]         
+    wire [3:0]   awregion;    // [3:0]         
+    wire [3:0]   awqos;       // [3:0]         
+    wire [7:0]   awuser;      // [7:0]         
+    
+    wire         wready;                       
+    wire         wvalid;                       
+    wire         wlast;                        
+    wire [5:0]   wid;                // [3:0]  
+    wire [127:0] wdata;            // [31:0]    
+    wire [15:0]  wstrb ;   // [15:0] 
 
-   wire 	    bvalid;
-   wire [ 1:0] 	bresp;
-   wire [ 5:0] 	bid;
-   wire 	    bready;
+    wire         bvalid;
+    wire [ 1:0]  bresp;
+    wire [ 5:0]  bid;
+    wire         bready;
 
-   //Replace regexp (default \(\w+\)\(,\) -> \1^I^I(\1^I),^J): 
-   TB_EVA eva(// Outputs
-			  .htrans			(htrans		),
-			  .hwrite			(hwrite		),
-			  .haddr			(haddr		),
-			  .hwdata			(hwdata		),
-			  .hsize			(hsize		),
-			  .hburst			(hburst		),
-			  .hprot			(hprot		),
-			  .hready_out		(hready_out	),
+    //Replace regexp (default \(\w+\)\(,\) -> \1^I^I(\1^I),^J): 
+    TB_EVA eva(// Outputs
+			   .htrans	   (htrans	   ),
+			   .hwrite	   (hwrite	   ),
+			   .haddr	   (haddr	   ),
+			   .hwdata	   (hwdata	   ),
+			   .hsize	   (hsize	   ),
+			   .hburst	   (hburst	   ),
+			   .hprot	   (hprot	   ),
+			   .hready_out (hready_out ),
 
-			  .arready			(arready	),
-			  .rvalid			(rvalid		),
-			  .rid				(rid		),
-			  .ruser			(ruser      ),
-			  .rdata			(rdata		),
-			  .rlast			(rlast		),
-			  .awready			(awready	),
-			  .wready			(wready		),
+			   .arready	   (arready    ),
+			   .rvalid	   (rvalid	   ),
+			   .rid		   (rid        ),
+			   .ruser	   (ruser      ),
+			   .rdata	   (rdata	   ),
+			   .rlast	   (rlast	   ),
+			   .awready	   (awready    ),
+			   .wready	   (wready	   ),
 
-			  // Inputs
-			  .hclk				(hclk		),
-			  .hrest_n			(hrest_n	),
-			  .hready_in		(hready_in	),
-			  .hresp			(hresp		),
-			  .hrdata			(hrdata		),
-			  .aclk				(aclk		),
-			  .arest_n			(arest_n	),
-			  .arvalid			(arvalid	),
+			   // Inputs
+			   .hclk	   (hclk	   ),
+			   .hrest_n	   (hrest_n    ),
+			   .hready_in  (hready_in  ),
+			   .hresp	   (hresp	   ),
+			   .hrdata	   (hrdata	   ),
+			   .aclk	   (aclk	   ),
+			   .arest_n	   (arest_n    ),
+			   .arvalid	   (arvalid    ),
 
-			  .arid				(arid		),
-			  .araddr			(araddr		),
-			  .arlen			(arlen		),
-			  .arsize			(arsize		),
-			  .arburst			(arburst	),
-			  .arlock			(arlock		),
-			  .arcache			(arcache	),
-			  .arprot			(arprot		),
+			   .arid	   (arid	   ),
+			   .araddr	   (araddr	   ),
+			   .arlen	   (arlen	   ),
+			   .arsize	   (arsize	   ),
+			   .arburst	   (arburst    ),
+			   .arlock	   (arlock	   ),
+			   .arcache	   (arcache    ),
+			   .arprot	   (arprot	   ),
 
-			  .arregion			(arregion	),
-			  .arqos			(arqos		),
-			  .aruser			(aruser		),
-			  .rready			(rready		),
-			  .rresp			(rresp		),
-			  .awvalid			(awvalid	),
-			  .awid				(awid		),
-			  .awaddr			(awaddr		),
+			   .arregion   (arregion   ),
+			   .arqos	   (arqos	   ),
+			   .aruser	   (aruser	   ),
+			   .rready	   (rready	   ),
+			   .rresp	   (rresp	   ),
+			   .awvalid	   (awvalid    ),
+			   .awid	   (awid	   ),
+			   .awaddr	   (awaddr	   ),
 
-			  .awlen			(awlen		),
-			  .awsize			(awsize		),
-			  .awburst			(awburst	),
-			  .awlock			(awlock		),
-			  .awcache			(awcache	),
-			  .awprot			(awprot		),
-			  .awregion			(awregion	),
-			  .awqos			(awqos		),
+			   .awlen	   (awlen	   ),
+			   .awsize	   (awsize	   ),
+			   .awburst	   (awburst    ),
+			   .awlock	   (awlock	   ),
+			   .awcache	   (awcache    ),
+			   .awprot	   (awprot	   ),
+			   .awregion   (awregion   ),
+			   .awqos	   (awqos	   ),
 
-			  .awuser			(awuser		),
-			  .wvalid			(wvalid		),
-			  .wlast			(wlast		),
-			  .wid				(wid		),
-			  .wdata			(wdata		),
-			  .wstrb			(wstrb		),
+			   .awuser	   (awuser	   ),
+			   .wvalid	   (wvalid	   ),
+			   .wlast	   (wlast	   ),
+			   .wid		   (wid        ),
+			   .wdata	   (wdata	   ),
+			   .wstrb	   (wstrb	   ),
 
-			  .bvalid			(bvalid		),
-			  .bresp			(bresp		),
-			  .bid				(bid		),
-			  .bready			(bready		)
-			  );
-
-   TB_EVA_INTR U_EVA_INTR(
-			  // Inputs
-			  .clk			(hclk),
-			  .rst_n		(hrest_n),
-			  .interrupt		({31'b0, U_IVS_TOP.U_IVS_SLV.sw_rst})
-			  );
-   
-   
-   IVS_CLK_GEN ivs_clk_gen(
-			   // Outputs
-			   .aclk	(aclk	),
-			   .hclk	(hclk	),
-			   .arst_n	(arest_n),
-			   .hrst_n	(hrest_n)
+			   .bvalid	   (bvalid	   ),
+			   .bresp	   (bresp	   ),
+			   .bid		   (bid        ),
+			   .bready	   (bready	   )
 			   );
 
-   IVS_TOP U_IVS_TOP(// Outputs
-		     .hready_out	(hready_in	),
-		     .hresp		(hresp	),
-		     .hrdata		(hrdata	),
-		     // Inputs
-		     .hclk		(hclk	),
-		     .hrst_n		(hrest_n	),
-		     .hsel		(1'b1	),
-		     .htrans		(htrans	),
-		     .hwrite		(hwrite	),
-		     .haddr		(haddr	),
-		     .hwdata		(hwdata	),
-		     .hsize		(hsize	),
-		     .hburst		(hburst	),
-		     .hprot		(hprot	),
-		     .hready_in		(hready_out	)
-		     	);
+    TB_EVA_INTR U_EVA_INTR(
+			               // Inputs
+			               .clk			(hclk),
+			               .rst_n		(hrest_n),
+			               .interrupt	({31'b0, U_IVS_TOP.U_IVS_SLV.sw_rst})
+			               );
+    
+    
+    IVS_CLK_GEN ivs_clk_gen(
+			                // Outputs
+			                .aclk	(aclk	),
+			                .hclk	(hclk	),
+			                .arst_n	(arest_n),
+			                .hrst_n	(hrest_n)
+			                );
 
-   EVA_MEM_WRAP #(32,64,1) U_RAM32x64(// Outputs
-				      .rdata		(hrdata	),
-				      // Inputs
-				      .clk		(hclk	),
-				      .rst_n		(hrest_n	),
-				      .rd		(rd	),
-				      .we		(we	),
-				      .addr		(addr	),
-				      .wdata		(wdata	)
-				      	);
-   
+    IVS_TOP U_IVS_TOP(// Outputs
+		              .hready_out (hready_in	),
+		              .hresp	  (hresp	),
+		              .hrdata	  (hrdata	),
+		              // Inputs
+		              .hclk       (hclk	),
+		              .hrst_n	  (hrest_n	),
+		              .hsel       (1'b1	),
+		              .htrans	  (htrans	),
+		              .hwrite	  (hwrite	),
+		              .haddr	  (haddr	),
+		              .hwdata	  (hwdata	),
+		              .hsize	  (hsize	),
+		              .hburst	  (hburst	),
+		              .hprot	  (hprot	),
+		              .hready_in  (hready_out	)
+		     	      );
+
+    EVA_MEM_WRAP #(32,64,1) U_RAM32x64(// Outputs
+				                       .rdata		(hrdata	),
+				                       // Inputs
+				                       .clk		(hclk	),
+				                       .rst_n		(hrest_n	),
+				                       .rd		(rd	),
+				                       .we		(we	),
+				                       .addr		(addr	),
+				                       .wdata		(wdata	)
+				      	               );
+    
 endmodule // TH
 
 // Local Variables:
